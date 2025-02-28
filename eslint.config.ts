11b1a07fff1e57722a17a7d71fdd2d7ab9c8cdcb
Oliver Davies <oliver.davies@lcp.uk.com>
2025-02-28 23:04:16 +0000

    initialise new vue 3 project

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+)
--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,15 @@
 <template>
-  <div id="app">
-    <heading class="heading-component"/>
-    <coin-table class="content-component"/>
-    <bottom class="bottom-component"/>
+  <div>
+    CoinTrends
   </div>
 </template>
 
-<script>
-import heading from '@/components/heading.vue';
-import coinTable from '@/components/coinTable.vue';
-import bottom from '@/components/bottom.vue';
+<script setup lang="ts">
 
-export default {
-  name: 'app',
-  components: {
-    heading: heading,
-    coinTable: coinTable,
-    bottom: bottom,
-  },
-};
 </script>
 
-<style lang="scss">
-@import "assets/css/minireset.css";
-@import "assets/css/variables.scss";
-@import "assets/css/global.scss";
-#app {
-  font-family: 'rawline', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: $textColor;
-  font-size: 16px;
-  box-sizing: border-box;
-  position: relative;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  @media (max-width: $breakpoint-med) {
-    font-size: 14px;
-  }
-  @media (max-width: $breakpoint-small) {
-    font-size: 12px;
-  }
-  .content-component {
-    flex: 1 0 auto;
-  }
-}
+
+
+<style scoped lang="scss">
+
 </style>
