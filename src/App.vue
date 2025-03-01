f9d7a0201df87e28ad16f2b2df002c1975df7891
Oliver Davies <oliver.davies@lcp.uk.com>
2025-03-01 02:07:43 +0000

    add global scss, add basic site structure in App.vue including header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    CoinTrends
+  <div id="app-content">
+    <header>
+      <div><img src="@/assets/images/cointrends.svg" alt="CoinTrends logo"></div>
+      <h1>CoinTrends</h1>
+    </header>
+    <main>
+      <div>
+      </div>
+    </main>
+    <footer>
+    </footer>
   </div>
 </template>
 
@@ -8,8 +17,29 @@
 
 </script>
 
-
-
 <style scoped lang="scss">
-
+#app-content {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  height: 100%;
+  font-size: 16px;
+  font-family: rawline, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  color: $colorText;
+}
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    display: block;
+    height: 2rem;
+    margin: auto;
+    margin-right: 0.5rem;
+  }
+  h1 {
+    font-family: 'Changa', sans-serif;
+    font-size: 2.5rem;
+  }
+}
 </style>
