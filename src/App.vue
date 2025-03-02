3831bba123cc42006f6e28e62a70dd018cb86259
Oliver Davies <oliver.davies@lcp.uk.com>
2025-03-02 15:53:56 +0000

    move site header into its own component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,6 @@
 <template>
   <div id="app-content">
-    <header>
-      <div class="header-left">
-        <img src="@/assets/images/cointrends.svg" alt="CoinTrends logo">
-      </div>
-      <h1>CoinTrends</h1>
-      <div class="header-right">
-        <div id="portfolio-links">
-          <a
-            href="https://oliverdavies.me/"
-            title="Oliver Davies | Portfolio"
-            target="_blank"
-          >oliverdavies.me</a>
-          <a
-            class="github-link"
-            href="https://github.com/oliver-0/CoinTrends"
-            title="oliver-0/CoinTrends"
-            target="_blank"
-          ><span class="footer-resources-text">CoinTrends on GitHub</span></a>
-        </div>
-      </div>
-    </header>
+    <site-header />
     <main>
       <div />
       <site-footer />
@@ -29,6 +9,7 @@
 </template>
 
 <script setup lang="ts">
+import SiteHeader from '@/components/SiteHeader.vue';
 import SiteFooter from '@/components/SiteFooter.vue';
 </script>
 
@@ -42,40 +23,6 @@ import SiteFooter from '@/components/SiteFooter.vue';
   font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   color: $colorText;
-  header {
-    display: grid;
-    grid-template-columns: 1fr auto 1fr;
-    align-items: center;
-    .header-left {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      img {
-        height: 2rem;
-        margin-right: 0.5rem;
-      }
-    }
-    h1 {
-      font-family: 'Changa', sans-serif;
-      font-size: 2.5rem;
-    }
-    .header-right {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      #portfolio-links {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-        a {
-          color: #292929;
-          font-weight: 500;
-          font-size: 0.875rem;
-          margin-right: 1.5rem;
-        }
-      }
-    }
-  }
   main {
     display: grid;
     grid-template-rows: 1fr auto;
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+    <div class="header-left">
+      <img src="@/assets/images/cointrends.svg" alt="CoinTrends logo">
+    </div>
+    <h1>CoinTrends</h1>
+    <div class="header-right">
+      <div id="portfolio-links">
+        <a
+          href="https://oliverdavies.me/"
+          title="Oliver Davies | Portfolio"
+          target="_blank"
+        >oliverdavies.me</a>
+        <a
+          class="github-link"
+          href="https://github.com/oliver-0/CoinTrends"
+          title="oliver-0/CoinTrends"
+          target="_blank"
+        ><span class="footer-resources-text">CoinTrends on GitHub</span></a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+header {
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  align-items: center;
+  .header-left {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    img {
+      height: 2rem;
+      margin-right: 0.5rem;
+    }
+  }
+  h1 {
+    font-family: 'Changa', sans-serif;
+    font-size: 2.5rem;
+  }
+  .header-right {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    #portfolio-links {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      a {
+        color: #292929;
+        font-weight: 500;
+        font-size: 0.875rem;
+        margin-right: 1.5rem;
+      }
+    }
+  }
+}
+</style>
