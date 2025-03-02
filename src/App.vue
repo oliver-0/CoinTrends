3c55e8302140156cd5616f543d03fcd752b606be
Oliver Davies <oliver.davies@lcp.uk.com>
2025-03-02 15:49:00 +0000

    add Portfolio and GitHub links to site header, change base font to Open Sans, rework page layout and header layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app-content">
     <header>
-      <div><img src="@/assets/images/cointrends.svg" alt="CoinTrends logo"></div>
+      <div class="header-left">
+        <img src="@/assets/images/cointrends.svg" alt="CoinTrends logo">
+      </div>
       <h1>CoinTrends</h1>
+      <div class="header-right">
+        <div id="portfolio-links">
+          <a
+            href="https://oliverdavies.me/"
+            title="Oliver Davies | Portfolio"
+            target="_blank"
+          >oliverdavies.me</a>
+          <a
+            class="github-link"
+            href="https://github.com/oliver-0/CoinTrends"
+            title="oliver-0/CoinTrends"
+            target="_blank"
+          ><span class="footer-resources-text">CoinTrends on GitHub</span></a>
+        </div>
+      </div>
     </header>
     <main>
       <div />
+      <site-footer />
     </main>
-    <site-footer />
   </div>
 </template>
 
@@ -18,26 +35,51 @@ import SiteFooter from '@/components/SiteFooter.vue';
 <style scoped lang="scss">
 #app-content {
   display: grid;
-  grid-template-rows: auto 1fr auto;
+  grid-template-rows: auto 1fr;
+  overflow: auto;
   height: 100%;
   font-size: 16px;
-  font-family: rawline, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   color: $colorText;
-}
-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  img {
-    display: block;
-    height: 2rem;
-    margin: auto;
-    margin-right: 0.5rem;
+  header {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    .header-left {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      img {
+        height: 2rem;
+        margin-right: 0.5rem;
+      }
+    }
+    h1 {
+      font-family: 'Changa', sans-serif;
+      font-size: 2.5rem;
+    }
+    .header-right {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      #portfolio-links {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        a {
+          color: #292929;
+          font-weight: 500;
+          font-size: 0.875rem;
+          margin-right: 1.5rem;
+        }
+      }
+    }
   }
-  h1 {
-    font-family: 'Changa', sans-serif;
-    font-size: 2.5rem;
+  main {
+    display: grid;
+    grid-template-rows: 1fr auto;
+    overflow: auto;
   }
 }
 </style>
